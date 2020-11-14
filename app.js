7f9a4f8461d0f44faf4775399e8ded0af5e2a6e0
L. Haeller <lhaeller@tuta.io>
2020-11-14 13:39:04 +0100

    field auto-generated

--- app.js
+++ app.js
@@ -1,61 +1,45 @@
-// file for JavaScript
-document.addEventListener("DOMContentLoaded", (event) => {
-    console.log('DOM is ready.')
-    let itemA = document.getElementsByClassName("itemA")[0];
-    
-    console.log(itemA.style);
+// automatically generate html for playboard
+const generatePlayboard = function(){
 
-    console.log("my grid area is = " + itemA.style.gridArea)
-
-    
-    
+    // for n fields set grid-lines 
     /**  objective
      *  { "grid-row-start" → "1", "grid-row-end" → "5", "grid-column-start" → "1", "grid-column-end" → "5" }
      *  */ 
 
-    let style = window.getComputedStyle(itemA);
+    let playboard = document.getElementById("playboard");
 
-    let xStart = style.getPropertyValue('grid-row-start');
-    let xEnd = style.getPropertyValue('grid-row-end');
-    let yStart = style.getPropertyValue('grid-column-start');
-    let yEnd = style.getPropertyValue('grid-column-end');
+    let xCoordinate = 1;
+    let yCoordinate = 1;
 
-    let formerCoordinates = `${yStart} / ${xStart} / ${yEnd} / ${xEnd}`
-    console.log(`formerCoordinates is = ${formerCoordinates}`);
-    
-    /** objective
-     *  newCoordinates =  `1 / 1 / 5 / 5`;
-     */
-    console.log("changing style...");
-    
-    xStartNew = 1;
-    yStartNew = 1;
-    xEndNew = 5;
-    yEndNew = 5;
+    let numberOfFields = 100;
+    let allFields = [];
 
-    style.setProperty('grid-row-start',xStartNew);
-    style.setProperty('grid-row-end',yStartNew);
-    style.setProperty('grid-column-start',xEndNew);
-    style.setProperty('grid-column-end',yEndNew);
+    for(let field = 1;field <= numberOfFields;field++){
 
-    // check for new coordinates
-    xStart = style.getPropertyValue('grid-row-start');
-    xEnd = style.getPropertyValue('grid-row-end');
-    yStart = style.getPropertyValue('grid-column-start');
-    yEnd = style.getPropertyValue('grid-column-end');
-    let newCoordinates = `${yStart} / ${xStart} / ${yEnd} / ${xEnd}`
+        // let html = `<div class="item">${field}</div>`;
+        let element = document.createElement('div');
+        element.classList.add("item");
+        element.innerHTML = `${field}`;
+        //playboard.innerHTML = html;
+        playboard.appendChild(element);
 
-    if (formerCoordinates != newCoordinates){
-        console.log(`%c passed! ${formerCoordinates} != ${newCoordinates}`,'color:green;');
-    }else{
-        console.log(`%c failed!!!!!!! ${formerCoordinates} == ${newCoordinates}`,'color:red;');
+
     }
 
-        /* co-ord. Y-start / X-start / Y-end / X-end */    
-    
-        
-        console.log(`%c itemA has grid area = ${yStart} / ${xStart} / ${yEnd} / ${xEnd} `,'color: green; font-style: bold;') 
-    itemA.style.gridArea = "1 / 1 / 5 / 5";
-    
+    // set properties
+    /**
+     *  style.setProperty('grid-row-start',xStartNew);
+        style.setProperty('grid-row-end',yStartNew);
+        style.setProperty('grid-column-start',xEndNew);
+        style.setProperty('grid-column-end',yEndNew);
+     */
 
-});
+}
+
+
+// find out neighbours (of a field)
+const findOutNeighbours = function(){
+
+}
+
+generatePlayboard();
---
+++ code-snippets.js
@@ -0,0 +1,61 @@
+// file for JavaScript
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('DOM is ready.')
+    let itemA = document.getElementsByClassName("itemA")[0];
+    
+    console.log(itemA.style);
+
+    console.log("my grid area is = " + itemA.style.gridArea)
+
+    
+    
+    /**  objective
+     *  { "grid-row-start" → "1", "grid-row-end" → "5", "grid-column-start" → "1", "grid-column-end" → "5" }
+     *  */ 
+
+    let style = window.getComputedStyle(itemA);
+
+    let xStart = style.getPropertyValue('grid-row-start');
+    let xEnd = style.getPropertyValue('grid-row-end');
+    let yStart = style.getPropertyValue('grid-column-start');
+    let yEnd = style.getPropertyValue('grid-column-end');
+
+    let formerCoordinates = `${yStart} / ${xStart} / ${yEnd} / ${xEnd}`
+    console.log(`formerCoordinates is = ${formerCoordinates}`);
+    
+    /** objective
+     *  newCoordinates =  `1 / 1 / 5 / 5`;
+     */
+    console.log("changing style...");
+    
+    xStartNew = 1;
+    yStartNew = 1;
+    xEndNew = 5;
+    yEndNew = 5;
+
+    style.setProperty('grid-row-start',xStartNew);
+    style.setProperty('grid-row-end',yStartNew);
+    style.setProperty('grid-column-start',xEndNew);
+    style.setProperty('grid-column-end',yEndNew);
+
+    // check for new coordinates
+    xStart = style.getPropertyValue('grid-row-start');
+    xEnd = style.getPropertyValue('grid-row-end');
+    yStart = style.getPropertyValue('grid-column-start');
+    yEnd = style.getPropertyValue('grid-column-end');
+    let newCoordinates = `${yStart} / ${xStart} / ${yEnd} / ${xEnd}`;
+
+    if (formerCoordinates != newCoordinates){
+        console.log(`%c passed! ${formerCoordinates} != ${newCoordinates}`,'color:green;');
+    }else{
+        console.log(`%c failed!!!!!!! ${formerCoordinates} == ${newCoordinates}`,'color:red;');
+    }
+
+        /* co-ord. Y-start / X-start / Y-end / X-end */    
+    
+        
+    console.log(`%c itemA has grid area = ${yStart} / ${xStart} / ${yEnd} / ${xEnd} `,'color: green; font-style: bold;') 
+    itemA.style.gridArea = "1 / 1 / 5 / 5";
+    
+
+});
